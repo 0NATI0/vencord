7c64e60908d7894af1aa2aa6721ff3a3607e7fb4
Ilusion <107504334+0NATI0@users.noreply.github.com>
2023-04-28 08:50:49 +0200

    Create liblary.lua

---
+++ liblary.lua
@@ -0,0 +1,10 @@
+-- Create a new library
+local MyLibrary = {}
+
+-- Define a function in the library
+function MyLibrary.printMessage(message)
+    print(message)
+end
+
+-- Return the library
+return MyLibrary

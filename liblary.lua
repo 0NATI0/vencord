a43b50b2771f1907a976cf361621f108205e2d89
Ilusion <107504334+0NATI0@users.noreply.github.com>
2023-04-28 08:55:22 +0200

    Update liblary.lua

--- liblary.lua
+++ liblary.lua
@@ -1,10 +1,67 @@
--- Create a new library
-local MyLibrary = {}
+-- Define the library
+local MyUI = {}
 
--- Define a function in the library
-function MyLibrary.printMessage(message)
-    print(message)
+-- Define the classes
+local Window = {}
+local Tab = {}
+local Section = {}
+
+-- Window constructor
+function Window.new(title)
+    local self = {}
+    self.Title = title
+    self.Tabs = {}
+    
+    function self:NewTab(tabTitle)
+        local tab = Tab.new(tabTitle)
+        table.insert(self.Tabs, tab)
+        return tab
+    end
+    
+    return self
+end
+
+-- Tab constructor
+function Tab.new(title)
+    local self = {}
+    self.Title = title
+    self.Sections = {}
+    
+    function self:NewSection(sectionTitle)
+        local section = Section.new(sectionTitle)
+        table.insert(self.Sections, section)
+        return section
+    end
+    
+    return self
+end
+
+-- Section constructor
+function Section.new(title)
+    local self = {}
+    self.Title = title
+    
+    function self:AddButton(buttonText, onClick)
+        -- Code to add a button to the section goes here
+    end
+    
+    function self:AddTextBox(promptText, onSubmit)
+        -- Code to add a text box to the section goes here
+    end
+    
+    return self
+end
+
+-- CreateLib function
+function MyUI.CreateLib(title)
+    local window = Window.new(title)
+    
+    function window:Render()
+        -- Code to render the UI window goes here
+    end
+    
+    return window
 end
 
 -- Return the library
-return MyLibrary
+return MyUI
